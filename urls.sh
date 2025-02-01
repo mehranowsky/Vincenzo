3267dc54f04823bc46a5a6efb76052a60818d069
mehranowsky <mehrnowski@gmail.com>
2025-02-01 16:29:58 +0330

    final

--- urls.sh
+++ urls.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 DOMAIN="$1"
 USE_KATANA="$2"
 
-$SCRIPT_DIR/module/wayback.sh "$DOMAIN"
+$SCRIPT_DIR/modules/wayback.sh "$DOMAIN"
 if [ "$USE_KATANA" = true ]; then
     echo "Running Katana tool..."
-    $SCRIPT_DIR/module/katana.sh "$DOMAIN" && sort -u data/wayback.txt data/katana.txt > data/urls.txt && rm data/katana.txt
+    $SCRIPT_DIR/modules/katana.sh "$DOMAIN" && sort -u data/wayback.txt data/katana.txt > data/urls.txt && rm data/katana.txt
 fi
