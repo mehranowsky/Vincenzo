7ba12326a0cd005c0b32a11a68e94dd0a6e61761
mehranowsky <mehrnowski@gmail.com>
2025-02-02 14:49:50 +0330

    params.txt

--- params.sh
+++ params.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 USE_KATANA="$2"
-fallparams -u data/wayback.txt -silent
+fallparams -u data/wayback.txt -silent -o data/params.txt
 if [ "$USE_KATANA" = true ]; then
-    cat data/urls.txt | unfurl keys | anew -q data/parameters.txt
+    cat data/urls.txt | unfurl keys | anew -q data/params.txt
 else
-    cat data/wayback.txt | unfurl keys | anew -q data/parameters.txt
+    cat data/wayback.txt | unfurl keys | anew -q data/params.txt
 fi
