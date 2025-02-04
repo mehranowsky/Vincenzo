ded560bd0b4673a6eb78aa270de477f48736f4be
mehranowsky <mehrnowski@gmail.com>
2025-02-04 13:10:53 +0330

    katana cookie

--- modules/crawl.sh
+++ modules/crawl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DOMAIN=$1
+COOKIE=$2
 # Passive crawl
-echo "$1" | waybackurls | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|f4v|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u > data/wayback.txt
-gau "$1" --threads 1 | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|fit|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u >> data/wayback.txt
+echo "$DOMAIN" | waybackurls | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|f4v|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u > data/wayback.txt
+gau "$DOMAIN" --threads 1 | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|fit|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u >> data/wayback.txt
 
 # Active crawl
-echo "$1" | katana -jc -automatic-form-fill -silent -headless -system-chrome -crawl-scope "$1" -extension-filter css,jpg,jpeg,png,svg,img,gif,mp4,flv,pdf,doc,ogv,webm,wmv,webp,mov,mp3,m4a,m4p,ppt,pptx,scss,tif,tiff,ttf,otf,woff,woff2,bmp,ico,eot,htc,swf,rtf,image > data/katana.txt
+echo "$DOMAIN" | katana -jc -automatic-form-fill -silent -H "$COOKIE" -headless -system-chrome -crawl-scope "$1" -extension-filter css,jpg,jpeg,png,svg,img,gif,mp4,flv,pdf,doc,ogv,webm,wmv,webp,mov,mp3,m4a,m4p,ppt,pptx,scss,tif,tiff,ttf,otf,woff,woff2,bmp,ico,eot,htc,swf,rtf,image > data/katana.txt
--- vini.sh
+++ vini.sh
@@ -3,21 +3,23 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 mkdir data
 
-echo -e "\e[35m-------GO AHEAD MR.PIPINO-------\e[0m"
+echo -e "\e[35m-----GO AHEAD MR.PIPINO-----\e[0m"
 
 # Initialize variables
 DOMAIN=""
 MODE=""
+COOKIE=""
 
 # Parse options
-while getopts "d:upa" option; do
+while getopts "d:upac" option; do
     case $option in
-        d) DOMAIN=$OPTARG ;;
-        u) MODE="u" ;;
-        p) MODE="p" ;;
-        a) MODE="a" ;;
+        d) DOMAIN=$OPTARG ;;    # Set DOMAIN
+        u) MODE="u" ;;          # Set mode to URLs
+        p) MODE="p" ;;          # Set mode to Params
+        a) MODE="a" ;;          # Set mode to All
+        c) COOKIE="$OPTARG" ;;  # Set COOKIE
         *) 
-            echo "Usage: $0 [-u] URLs mode [-p] Params mode [-d] Target domain [-a] All"
+            echo "Usage: $0 [-u] URLs mode [-p] Params mode [-d] Target domain [-a] All [-c] Cookie"
             exit 1
             ;;
     esac
@@ -33,7 +35,7 @@ fi
 case $MODE in
     u)
         echo -e "\e[31m************URLs************\e[0m"
-        $SCRIPT_DIR/urls.sh "$DOMAIN"
+        $SCRIPT_DIR/urls.sh "$DOMAIN" "$COOKIE"
         ;;
     p)
         echo -e "\e[31m***********Params***********\e[0m"
@@ -41,7 +43,7 @@ case $MODE in
         ;;
     a)
         echo -e "\e[31m************URLs************\e[0m"
-        $SCRIPT_DIR/urls.sh "$DOMAIN"
+        $SCRIPT_DIR/urls.sh "$DOMAIN" "$COOKIE"
         echo -e "\e[31m***********Params***********\e[0m"
         $SCRIPT_DIR/params.sh "$DOMAIN"
         ;;
